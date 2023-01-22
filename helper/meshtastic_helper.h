ee0df224e81f6cdd134ad3c007211d522f9c97f0
Carrow Shino <carrowsh@student.21-school.ru>
2023-01-22 21:30:42 +0300

    first module commit

---
+++ helper/meshtastic_helper.h
@@ -0,0 +1,42 @@
+#ifndef MESHTASTIC_HELPER_H
+#define MESHTASTIC_HELPER_H
+
+#include "ns3/lora-net-device.h"
+#include "ns3/lora-packet-tracker.h"
+#include "ns3/lora-phy-helper.h"
+#include "ns3/lorawan-mac-helper.h"
+#include "ns3/net-device-container.h"
+
+// #include "ns3/net-device.h"
+// #include "ns3/node-container.h"
+
+// #include <ctime>
+
+namespace ns3
+{
+namespace meshtastic
+{
+
+class LoraMeshHelper
+{
+  public:
+    ~LoraMeshHelper();
+    LoraMeshHelper();
+
+    NetDeviceContainer Install(const LoraMeshHelper& meshHelper,
+                               const lorawan::LoraPhyHelper& phyHelper,
+                               const lorawan::LorawanMacHelper& macHelper,
+                               NodeContainer c) const;
+    NetDeviceContainer Install(const LoraMeshHelper& meshHelper,
+                               const lorawan::LoraPhyHelper& phyHelper,
+                               const lorawan::LorawanMacHelper& macHelper,
+                               Ptr<Node> node) const;
+
+    lorawan::LoraPacketTracker* m_packetTracker = 0;
+};
+
+} // namespace meshtastic
+
+} // namespace ns3
+
+#endif /* MESHTASTIC_HELPER_H */
